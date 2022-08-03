6272b0d343026e6939c81d30e733090e6f3495ce
jblack97 <76516724+jblack97@users.noreply.github.com>
2022-08-03 17:02:56 +0100

    Update convert_models.sh

--- convert_models.sh
+++ convert_models.sh
@@ -1,19 +1,17 @@
-
-
-! python convert_model.py --tf_checkpoint /tf_models/BERT_SND_epoch_0_step_194828/bert_model_epoch_0_step_194828.ckpt-194828 \
+python convert_model.py --tf_checkpoint /tf_models/BERT_SND_epoch_0_step_194828/bert_model_epoch_0_step_194828.ckpt-194828 \
 --config_file tf_models/bert_config.json --pytorch_dump_path pytorch_models/pytorch_model_SND.bin
 
-! python convert_model.py --tf_checkpoint /tf_models/BERT_RUR_epoch_0_step_194828/bert_model_epoch_0_step_194828.ckpt-194828 \
+python convert_model.py --tf_checkpoint /tf_models/BERT_RUR_epoch_0_step_194828/bert_model_epoch_0_step_194828.ckpt-194828 \
 --config_file tf_models/bert_config.json --pytorch_dump_path pytorch_models/pytorch_model_RUR.bin
 
-! python convert_model.py --tf_checkpoint /tf_models/BERT_MSUR_epoch_0_step_194828/bert_model_epoch_0_step_194828.ckpt-194828 \
+python convert_model.py --tf_checkpoint /tf_models/BERT_MSUR_epoch_0_step_194828/bert_model_epoch_0_step_194828.ckpt-194828 \
 --config_file tf_models/bert_config.json --pytorch_dump_path pytorch_models/pytorch_model_MSUR.bin
 
-! python convert_model.py --tf_checkpoint /tf_models/BERT_PCD_epoch_0_step_194828/bert_model_epoch_0_step_194828.ckpt-194828 \
+python convert_model.py --tf_checkpoint /tf_models/BERT_PCD_epoch_0_step_194828/bert_model_epoch_0_step_194828.ckpt-194828 \
 --config_file tf_models/bert_config.json --pytorch_dump_path pytorch_models/pytorch_model_PCD.bin
 
-! python convert_model.py --tf_checkpoint /tf_models/BERT_ISS_epoch_0_step_194828/bert_model_epoch_0_step_194828.ckpt-194828 \
+python convert_model.py --tf_checkpoint /tf_models/BERT_ISS_epoch_0_step_194828/bert_model_epoch_0_step_194828.ckpt-194828 \
 --config_file tf_models/bert_config.json --pytorch_dump_path pytorch_models/pytorch_model_ISS.bin
 
-! python convert_model.py --tf_checkpoint /tf_models/BERT_NSP_MLM_epoch_0_step_194828/bert_model_epoch_0_step_194828.ckpt-194828 \
+python convert_model.py --tf_checkpoint /tf_models/BERT_NSP_MLM_epoch_0_step_194828/bert_model_epoch_0_step_194828.ckpt-194828 \
 --config_file tf_models/bert_config.json --pytorch_dump_path pytorch_models/pytorch_model_NSP_MLM.bin
