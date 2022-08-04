80d12eddc338d780a3fa050b207c287ab7fc4e14
jblack97 <76516724+jblack97@users.noreply.github.com>
2022-08-04 11:02:08 +0100

    Update convert_models.sh

--- convert_models.sh
+++ convert_models.sh
@@ -7,7 +7,7 @@ python convert_model.py --tf_checkpoint /tf_models/BERT_RUR_epoch_0_step_80000/b
 python convert_model.py --tf_checkpoint /tf_models/BERT_MSUR_epoch_0_step_80000/bert_model_epoch_0_step_80000.ckpt-80000\
 --config_file tf_models/bert_config.json --pytorch_dump_path pytorch_models/pytorch_model_MSUR_80000.bin
 
-python convert_model.py --tf_checkpoint /tf_models/BERT_PCD_epoch_0_step_80000/bert_model_epoch_0_step_80000.ckpt-194828 \
+python convert_model.py --tf_checkpoint /tf_models/BERT_PCD_epoch_0_step_80000/bert_model_epoch_0_step_80000.ckpt-80000 \
 --config_file tf_models/bert_config.json --pytorch_dump_path pytorch_models/pytorch_model_PCD_80000.bin
 
 python convert_model.py --tf_checkpoint /tf_models/BERT_ISS_epoch_0_step_80000/bert_model_epoch_0_step_80000.ckpt-80000\
