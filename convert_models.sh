5bacf77f4d5ffbbe88a43227cd3e0b90b8134586
jblack97 <76516724+jblack97@users.noreply.github.com>
2022-08-11 16:52:44 +0100

    Update convert_models.sh

--- convert_models.sh
+++ convert_models.sh
@@ -1,17 +1,18 @@
-python convert_model.py --tf_checkpoint /tf_models/BERT_SND_epoch_0_step_80000/bert_model_epoch_0_step_80000.ckpt-80000\
---config_file tf_models/bert_config.json --pytorch_dump_path pytorch_models/pytorch_model_SND_80000.bin
-
-python convert_model.py --tf_checkpoint /tf_models/BERT_RUR_epoch_0_step_80000/bert_model_epoch_0_step_80000.ckpt-80000\
---config_file tf_models/bert_config.json --pytorch_dump_path pytorch_models/pytorch_model_RUR_80000.bin
-
-python convert_model.py --tf_checkpoint /tf_models/BERT_MSUR_epoch_0_step_80000/bert_model_epoch_0_step_80000.ckpt-80000\
---config_file tf_models/bert_config.json --pytorch_dump_path pytorch_models/pytorch_model_MSUR_80000.bin
-
-python convert_model.py --tf_checkpoint /tf_models/BERT_PCD_epoch_0_step_80000/bert_model_epoch_0_step_80000.ckpt-80000 \
---config_file tf_models/bert_config.json --pytorch_dump_path pytorch_models/pytorch_model_PCD_80000.bin
-
-python convert_model.py --tf_checkpoint /tf_models/BERT_ISS_epoch_0_step_80000/bert_model_epoch_0_step_80000.ckpt-80000\
---config_file tf_models/bert_config.json --pytorch_dump_path pytorch_models/pytorch_model_ISS_80000.bin
-
-python convert_model.py --tf_checkpoint /tf_models/BERT_NSP_MLM_epoch_0_step_80000/bert_model_epoch_0_step_80000.ckpt-80000 \
---config_file tf_models/bert_config.json --pytorch_dump_path pytorch_models/pytorch_model_NSP_MLM_80000.bin
+MODEL_NAME="${1}" NUM_EPOCHS="${2}"
+python convert_model.py --tf_checkpoint /tf_models/BERT_${MODEL_NAME}_epoch_0_step_${NUM_EPOCHS}/bert_model_epoch_0_step_${NUM_EPOCHS}.ckpt-${NUM_EPOCHS}\
+--config_file tf_models/bert_config.json --pytorch_dump_path pytorch_models/pytorch_model_${MODEL_NAME}_${NUM_EPOCHS}.bin
+NUM_EPOCHS="${3}"
+python convert_model.py --tf_checkpoint /tf_models/BERT_${MODEL_NAME}_epoch_0_step_${NUM_EPOCHS}/bert_model_epoch_0_step_${NUM_EPOCHS}.ckpt-${NUM_EPOCHS}\
+--config_file tf_models/bert_config.json --pytorch_dump_path pytorch_models/pytorch_${MODEL_NAME}_${NUM_EPOCHS}.bin
+NUM_EPOCHS="${4}"
+python convert_model.py --tf_checkpoint /tf_models/BERT_${MODEL_NAME}_epoch_0_step_${NUM_EPOCHS}/bert_model_epoch_0_step_${NUM_EPOCHS}.ckpt-${NUM_EPOCHS}\
+--config_file tf_models/bert_config.json --pytorch_dump_path pytorch_models/pytorch_model_${MODEL_NAME}_${NUM_EPOCHS}.bin
+NUM_EPOCHS="${5}"
+python convert_model.py --tf_checkpoint /tf_models/BERT_${MODEL_NAME}_epoch_0_step_${NUM_EPOCHS}/bert_model_epoch_0_step_${NUM_EPOCHS}.ckpt-${NUM_EPOCHS} \
+--config_file tf_models/bert_config.json --pytorch_dump_path pytorch_models/pytorch_model_${MODEL_NAME}_${NUM_EPOCHS}.bin
+NUM_EPOCHS="${6}"
+python convert_model.py --tf_checkpoint /tf_models/BERT_${MODEL_NAME}_epoch_0_step_${NUM_EPOCHS}/bert_model_epoch_0_step_${NUM_EPOCHS}.ckpt-${NUM_EPOCHS}\
+--config_file tf_models/bert_config.json --pytorch_dump_path pytorch_models/pytorch_model_${MODEL_NAME}_${NUM_EPOCHS}.bin
+NUM_EPOCHS="${7}"
+python convert_model.py --tf_checkpoint /tf_models/BERT_${MODEL_NAME}_epoch_0_step_${NUM_EPOCHS}/bert_model_epoch_0_step_${NUM_EPOCHS}.ckpt-${NUM_EPOCHS} \
+--config_file tf_models/bert_config.json --pytorch_dump_path pytorch_models/pytorch_model_${MODEL_NAME}_${NUM_EPOCHS}.bin
